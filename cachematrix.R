be622b4acec6f7f1d0d8050e1ff50d8af87cf87f
Curt Fuhlman <curt@fuhlman.com>
2014-04-24 11:26:08 -0700

    R Programming PA2
    Matrix caching functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions that cache the inverse of a matrix to 
+## help reduce costly re-computation
 
-## Write a short comment describing this function
-
+## Create a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
 
+  if (!is.matrix(x)) {
+    message("Invalid input")
+  }
+  else {
+    cached.x <<- x
+    cached.inv.x <<- solve(x)
+  }
 }
 
 
-## Write a short comment describing this function
-
+## Compute the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then 
+## retrieve the inverse from the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  if (!is.matrix(x)) {
+    message("Invalid input")
+  }
+  else if (identical(x, cached.x) && is.matrix(cached.inv.x)) {
+    message("Getting cached data")
+    cached.inv.x
+  }
+  else {
+    solve(x)
+  }
 }
